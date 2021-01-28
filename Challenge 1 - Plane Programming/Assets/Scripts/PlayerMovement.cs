57dd0402a0c10cacf335efd29b71a2ca8f4ee4a5
Jhwinder <fyuryusgamer@gmail.com>
2021-01-27 18:09:37 -0700

    Finished Challenge 1

---
+++ Challenge 1 - Plane Programming/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private float vInput = 10.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 65.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Gets input from controller
+        vInput = Input.GetAxis("Vertical");
+        // Creates speed of plane
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        transform.Rotate(Vector3.right * vInput * -1 * Time.deltaTime * turnSpeed);
+
+    }
+}
