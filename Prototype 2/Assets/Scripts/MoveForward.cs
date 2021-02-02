7486691c82f4b8b79356c3e8202f5041545456b1
Jhwinder <fyuryusgamer@gmail.com>
2021-02-01 17:22:16 -0700

    Started Prototype 2
    Added player controller + movement parameters. Created banana projectile.

---
+++ Prototype 2/Assets/Scripts/MoveForward.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveForward : MonoBehaviour
+{
+    public float speed = 15.0f;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(0 , 2, -4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
---
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+    private float hInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        if(transform.position.x < -15)
+        {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        }
+        if(transform.position.x > 15)
+        {
+            transform.position = new Vector3(15, transform.position.y, transform.position.z);
+        }
+        transform.Translate(Vector3.right * hInput * speed * Time.deltaTime);
+        
+    }
+}
