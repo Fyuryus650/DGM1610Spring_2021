8240cdf822bb583145f93d6fbec18a7992d79d1e
Jhwinder <fyuryusgamer@gmail.com>
2021-02-02 19:32:32 -0700

    Began work on Lab 1
    Created place holder player and set up camera follow script. Also created player movement script for player

---
+++ Lab Project 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 10.0f;
+    public float turnSpeed = 80.0f;
+    public float hInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        transform.Rotate(Vector3.up * hInput * turnSpeed * Time.deltaTime);
+
+    }
+}
