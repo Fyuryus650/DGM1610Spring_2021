afbaf5f0b3b23c3f03368917843658ad67d93977
Jhwinder <fyuryusgamer@gmail.com>
2021-02-14 12:55:41 -0700

    Lab 1/2 work
    Created prefabs and spawning positions, redefined the player boundaries.

--- Lab Project 1/Assets/Scripts/PlayerController.cs
+++ Lab Project 1/Assets/Scripts/PlayerController.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float turnSpeed = 80.0f;
+    public float turnSpeed = 15.0f;
+    public float acceleration = 4.0f;
     public float hInput;
+    public float vInput;
+
     private float xRange = 15.0f;
+    private float zRangeMax = 4.7f;
+    private float zRangeMin = -2.45f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +23,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
 
+        //Dictates the boundaries of the player on the screen
         if(transform.position.x > 15)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
@@ -26,8 +34,25 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
+        if(transform.position.z > zRangeMax)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeMax);
+        }
+        if(transform.position.z < zRangeMin)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRangeMin);
+        }
 
+        //basic player movement
         transform.Translate(Vector3.right * hInput * turnSpeed * Time.deltaTime);
+        transform.Translate(Vector3.forward * vInput * acceleration * Time.deltaTime);
+
+        //Creates an airbrake like effect where the plane has an easier time slowing down than it does accelerating
+        if (vInput < 0)
+        {
+            float newAcceleration = 5.0f;
+            transform.Translate(Vector3.forward * vInput * newAcceleration * Time.deltaTime);
+        }
 
     }
 }
