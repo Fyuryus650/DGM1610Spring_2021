3ea3353b8d281f76f6ff3762380b4b6c5e21bea0
Jhwinder <fyuryusgamer@gmail.com>
2021-02-17 17:19:53 -0700

    Prototype 3 started

---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRB;
+
+    public float jumpForce = 10;
+
+    public float gravityMod;
+
+    public bool isOnGround = true; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //&& makes the first part and second part required for the if statement to work
+        if(Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+
+        }
+    }
+    private void OnCollisionEnter(Collision other)
+    {
+        isOnGround = true;
+    }
+}
