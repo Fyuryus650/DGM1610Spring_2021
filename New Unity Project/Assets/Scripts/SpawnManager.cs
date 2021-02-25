8f9bbcd463f85340b6c0e2486ab45f00e2701d40
Jhwinder <fyuryusgamer@gmail.com>
2021-02-24 17:07:19 -0700

    Added destroy out of bouunds script

---
+++ New Unity Project/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject ObstaclePrefab;
+    public float startDelay = 2f;
+    public float repeatRate = 2f;
+
+    private Vector3 spawnPos = new Vector3(30, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("InvokeRepeating", startDelay, repeatRate);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void InvokeRepeating()
+    {
+        Instantiate(ObstaclePrefab, spawnPos, ObstaclePrefab.transform.rotation);
+    }
+}
